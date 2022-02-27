68826141245b3e18abce882f8d888d80131fecd1
Aftab-1998 <alam41976@gmail.com>
2022-02-27 15:23:05 +0530

    [AFTAB3] Add; Added The method to inser new contact details

--- AdvSQLQuery1.sql
+++ AdvSQLQuery1.sql
@@ -13,3 +13,18 @@ Zipcode varchar(30) not null,
 PhoneNumber varchar(20) not null,
 Email varchar(50) not null
 );
+
+----UC3 Ability to insert new contact address-----
+
+insert into Address_Book (FirstName, LastName, Address, City, State, ZipCode, PhoneNumber, Email) Values ('Aftab', 'Alam', 'Chhapkaiya', ' Brigunj', ' Parsa', '44300', '9819264250', 'aftab@gmail.com'),
+insert into Address_Book Values ('Sadam', 'Alam', 'Govindpura', ' Banglore', ' karnataka', '560045', '7975819623', 'sadam@gmail.com'),
+insert into Address_Book Values ('Hussain', 'Alam', 'Shivaji', ' Pune', ' Maharastra', '560048', '7975819452', 'Hussain@gmail.com')
+insert into Address_Book Values ('Ilyas', 'Alam', 'Gandak', ' parwanipur', ' kathmandu', '560049', '7455819623', 'ilyas@gmail.com');
+
+
+
+
+
+
+
+
