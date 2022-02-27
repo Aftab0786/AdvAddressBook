247faad08a5b4db9cadb9812d2d9405a0fa1b5ce
Aftab-1998 <alam41976@gmail.com>
2022-02-27 16:42:11 +0530

    [AFTAB5] Add; Added the method to delete the details of a person using their name

--- AdvSQLQuery1.sql
+++ AdvSQLQuery1.sql
@@ -16,18 +16,21 @@ Email varchar(50) not null
 
 ----UC3 Ability to insert new contact address-----
 
-insert into Address_Book (FirstName, LastName, Address, City, State, ZipCode, PhoneNumber, Email) Values ('Aftab', 'Alam', 'Chhapkaiya', ' Brigunj', ' Parsa', '44300', '9819264250', 'aftab@gmail.com'),
-insert into Address_Book Values ('Sadam', 'Alam', 'Govindpura', ' Banglore', ' karnataka', '560045', '7975819623', 'sadam@gmail.com'),
-insert into Address_Book Values ('Hussain', 'Alam', 'Shivaji', ' Pune', ' Maharastra', '560048', '7975819452', 'Hussain@gmail.com')
+insert into Address_Book (FirstName, LastName, Address, City, State, ZipCode, PhoneNumber, Email) Values ('Aftab', 'Alam', 'Chhapkaiya', ' Brigunj', ' Parsa', '44300', '9819264250', 'aftab@gmail.com');
+insert into Address_Book Values ('Sadam', 'Alam', 'Govindpura', ' Banglore', ' karnataka', '560045', '7975819623', 'sadam@gmail.com');
+insert into Address_Book Values ('Hussain', 'Alam', 'Shivaji', ' Pune', ' Maharastra', '560048', '7975819452', 'Hussain@gmail.com');
 insert into Address_Book Values ('Ilyas', 'Alam', 'Gandak', ' parwanipur', ' kathmandu', '560049', '7455819623', 'ilyas@gmail.com');
 
 ---UC4 Ability to edit existing contact ------
 
 SELECT * FROM Address_Book;
-update Address_Book set Zipcode = '5555', Email = 'alam41976@gmail.com' where FirstName = 'Sadam';
+update Address_Book set Zipcode = '5555', Email = 'alam41976@gmail.com' where FirstName = 'Hussain';
 select * from Address_Book;
 
+----UC5 Ability to Delete person Using person name----
 
+delete from Address_Book where FirstName = 'Aftab'
+select * from Address_Book;
 
 
 
